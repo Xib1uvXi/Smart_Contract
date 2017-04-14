820b8d6fc3937a100d532f9baa94512f89bc8c28
wangxi <wangxi@roadclouding.com>
2017-04-14 16:32:38 +0800

    ms

--- node-js/sign.js
+++ node-js/sign.js
@@ -1,8 +1,8 @@
 const Web3 = require('web3');
 const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 
-// const account = '0xfa3da03af4709506b56b26e409d1cdacf9d16fc5';
-const account = '0x8d0cf83e79593D3D3153EBEAcB001D6397A91Aca';
+const account1 = '0xfa3da03af4709506b56b26e409d1cdacf9d16fc5';
+const account2 = '0x8d0cf83e79593D3D3153EBEAcB001D6397A91Aca';
 
 const message = 'abc';
 
@@ -12,7 +12,7 @@ function messageHash(msg) {
 }
 
 function signHandler(err, signature) {
-    if (!err) {
+    if (!err) {       
         console.log('Signature:', signature);
         signature = signature.substr(2);
         r = '0x' + signature.substr(0, 64);
@@ -26,7 +26,7 @@ function signHandler(err, signature) {
         console.log('        v:', v)
         console.log();
         console.log('        messageHash:',messageHash(message));
-        process.exit();
+        // process.exit();
 
     } else {
         console.error('Coult not sign message:', err);
@@ -36,7 +36,8 @@ function signHandler(err, signature) {
 
 let messageHex = '0x' + Buffer.from(message).toString('hex');
 
-web3.eth.sign(account, messageHex, signHandler);
+web3.eth.sign(account1, messageHex, signHandler);
+web3.eth.sign(account2, messageHex, signHandler);
 
 
 //["0xfa3dA03Af4709506B56B26E409D1CDaCf9d16fC5","0x8d0cf83e79593D3D3153EBEAcB001D6397A91Aca"]
